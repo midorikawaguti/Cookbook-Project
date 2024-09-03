6c6d9c4e35cbd1a968b8af288a7baca5b7fd0795
Tatiana Kawaguti <midori.kawaguti@yahoo.com.br>
2024-09-03 15:52:38 -0400

    Added Header Component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 
-
-export default function Header() {
-   <>
-    <h1>Header</h1>
-   </>
+const Header = () => {
+  return (
+    <header>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">My Favorite Foods</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#recipes">Recipes</Nav.Link>
+            <NavDropdown title="Profile" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Saved
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">
+                Log out
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  )
 }
+
+export default Header
 
